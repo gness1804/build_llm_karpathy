11b1b36f84abef2e1445e3125e7e0483f7c784c2
Graham Nessler <graham.nessler@gmail.com>
2025-10-10 16:07:38 -0500

    Print batches of tokens to sample

--- model.py
+++ model.py
@@ -1,13 +1,14 @@
+import torch
+
+# core vars
+block_size = 8
+batch_size = 4 # how many independent sequences will we process in parallel?
+
 with open('sources/shakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
-    # print(text[:1000])
-
-# print(text[:1000])
 
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
-# print(''.join(chars))
-# print(vocab_size)
 
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
@@ -15,5 +16,42 @@ itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
-print(encode("hii there"))
-print(decode(encode("hii there")))
+data = torch.tensor(encode(text), dtype=torch.long)
+
+# Let's now split up the data into train and validation sets
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+inputs = train_data[:block_size]
+targets = train_data[1:block_size+1]
+for t in range(block_size):
+    context = inputs[:t+1]
+    target = targets[t]
+    # print(f"when input is {context} the target: {target}")
+
+torch.manual_seed(1337)
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+print('inputs:')
+print(xb.shape)
+print(xb)
+print('targets:')
+print(yb.shape)
+print(yb)
+
+print('----')
+
+for b in range(batch_size): # batch dimension
+    for t in range(block_size): # time dimension
+        context = xb[b, :t+1]
+        target = yb[b,t]
+        print(f"when input is {context.tolist()} the target: {target}")
