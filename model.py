aa946898548317172f2a514a78896e03e449d82b
Graham Nessler <graham.nessler@gmail.com>
2025-10-10 15:11:11 -0500

    feat: implement character encoding and decoding functions

--- model.py
+++ model.py
@@ -2,4 +2,18 @@ with open('sources/shakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
     # print(text[:1000])
 
-print(text[:1000])
+# print(text[:1000])
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+# print(''.join(chars))
+# print(vocab_size)
+
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+print(encode("hii there"))
+print(decode(encode("hii there")))
